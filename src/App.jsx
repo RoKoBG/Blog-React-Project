926b0498b128ea32d5e8b43e9538a611c662e880
Rosen Koychev <rosen.koichev@gmail.com>
2023-12-06 11:44:52 +0200

    Import firebase in the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
-
-
-  return (
-    <>
-        <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none gap-2">
-    <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-    </div>
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        </div>
-      </label>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-    </>
-  )
+    return (
+        <>
+            <ToastContainer />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/create" element={<createEdit />} />
+                <Route path="/update/:id" element={<createEdit />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/firebase/fb.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB-teyspnAB76mgJT3Z__68uXXhkjZjQDg",
+    authDomain: "simple-blog-a8b2d.firebaseapp.com",
+    projectId: "simple-blog-a8b2d",
+    storageBucket: "simple-blog-a8b2d.appspot.com",
+    messagingSenderId: "202549659428",
+    appId: "1:202549659428:web:54aeea118518c0308c15b1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore(app);
