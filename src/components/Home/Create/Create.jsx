adf8af0b27d49298918373dc4b14b2d06f0ed405
Rosen Koychev <rosen.koichev@gmail.com>
2023-12-18 13:44:12 +0200

    Implement Creation of Posts

--- src/components/Home/Create/Create.jsx
+++ src/components/Home/Create/Create.jsx
@@ -1,28 +1,18 @@
 import React from "react";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import Header from "../../Header/Header";
 import Footer from "../../Footer/Footer";
+import DemoPost from "./DemoPost";
 import { Blog } from "../../../contexts/context";
 import styles from "./Create.module.css";
 import ReactQuill from "react-quill";
 
 const Create = () => {
     const [text, setText] = useState("");
-    const [imgUrl, setImgUrl] = useState("");
+    const [title, setTitle] = useState("");
 
-    const imgRef = useRef(null);
-
-    const { post } = Blog();
-
-    const handleClick = () => {
-        imgRef.current.click();
-    };
-    const handleSubmit = () => {
-        try {
-            console.log(text, imgUrl);
-        } catch (error) {}
-    };
+    const { post, setPost } = Blog();
     return (
         <>
             <main className={styles.container}>
@@ -37,41 +27,15 @@ const Create = () => {
                                 <div className={styles["article-box"]}>
                                     <section className="w-[80%] md:w-[80%] lg:w[60%] mx-auto py-[3rem]">
                                         <input
-                                            onChange={(e) =>
-                                                setInput({
-                                                    ...input,
-                                                    title: e.target.value,
-                                                })
-                                            }
+                                            value={title}
+                                            onChange={(e) => {
+                                                setTitle(e.target.value);
+                                            }}
                                             type="text"
                                             placeholder="Title of the POST"
                                             className="text-2xl outline-none w-full p-[0.2rem] mb-4 shadow-xl border"
                                         />
-                                        <div
-                                            style={{
-                                                backgroundImage: `url(${imgUrl})`,
-                                            }}
-                                            onClick={handleClick}
-                                            className="w-full h-[200px] object-cover bg-gray-100 my-3 cursor-pointer bg-cover bg-no-repeat"
-                                        >
-                                            {!imgUrl && "Add Image"}
-                                        </div>
-                                        <input
-                                            onChange={(e) => {
-                                                setImgUrl(
-                                                    URL.createObjectURL(
-                                                        e.target.files[0]
-                                                    ),
-                                                    setInput({
-                                                        ...input,
-                                                        imgUrl: imgUrl,
-                                                    })
-                                                );
-                                            }}
-                                            ref={imgRef}
-                                            type="file"
-                                            hidden
-                                        />
+                                        <input type="file" hidden />
                                         <ReactQuill
                                             theme="bubble"
                                             value={text}
@@ -79,10 +43,26 @@ const Create = () => {
                                             placeholder="Write your post..."
                                             className="text-3xl outline-none  w-full p-[1rem] shadow-xl border"
                                         />
-                                        <button onClick={handleSubmit} className="border border-teal-600 bg-teal-200 py-2 mt-4 px-5 rounded-lg text-teal-700">
+                                        <button
+                                            onClick={() => setPost(true)}
+                                            className="border border-teal-600 bg-teal-200 py-2 mt-4 px-5 rounded-lg text-teal-700"
+                                        >
                                             Create
                                         </button>
                                     </section>
+                                    <div
+                                        className={`${
+                                            post
+                                                ? "visible opacity-100"
+                                                : "invisible opacity-0"
+                                        }`}
+                                    >
+                                        <DemoPost
+                                            setPost={setPost}
+                                            title={title}
+                                            text={text}
+                                        />
+                                    </div>
                                 </div>
                             </div>
                         </div>
